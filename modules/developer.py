d33ac3490c616df95e8c4bb54b879dae5b7f7ed3
h0r1zon <libertsxion@gmail.com>
2018-03-17 19:26:18 -0400

    .

--- modules/developer.py
+++ modules/developer.py
@@ -217,10 +217,6 @@ class Developer:
         restart_program()
 
 
-    async def on_message(self, message):
-        if 'nerd' in message.content.lower():
-            if message.author.id == 260024920757633025:
-                await message.channel.send("https://b1nzy.ratelimited.me/sm86b4.png")
 
 def setup(bot):
     bot.add_cog(Developer(bot))
