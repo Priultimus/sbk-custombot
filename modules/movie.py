de20db95ebbb84dd4a4e5f71b0816907e8271533
h0r1zon <libertsxion@gmail.com>
2018-04-13 15:52:16 -0400

    Fix up code a bit.

--- modules/movie.py
+++ modules/movie.py
@@ -1,22 +1,24 @@
-import json
 import discord
 from discord.ext import commands
-from __main__ import DataManager
-from __main__ import Checks
+from __main__ import DataManager, Checks
+
 
 class Movie:
-    @bot.event
-    async def on_message(message):
-        if message.channel.id == "426487069351608330":
-            DataManager.list_update('data/movie.json', 'movies', message.content)
 
     @Checks.is_staff()
     @commands.commands()
     async def movielist(self, ctx):
         movies = DataManager.read('data/movie.json')[str('movies')]
-        embed = discord.Embed(title='__Movie suggestions__:', description=movies, color=ctx.author.color)
-        #DataManager.delete('data/movie.json', 'movies')
+        embed = discord.Embed(title='__Movie suggestions__:',
+                              description=movies,
+                              color=ctx.author.color)
+        # DataManager.delete('data/movie.json', 'movies')
         await ctx.send(embed=embed)
+
+    async def on_message(message):
+        if message.channel.id == "426487069351608330":
+            DataManager.list_update('data/movie.json', 'movies', message.content)
+
 
 def setup(bot):
     bot.add_cog(Movie())
