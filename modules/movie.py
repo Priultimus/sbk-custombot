73e87fcdbf3c0f15463aaca78ca6974ab45a69c1
Antonio32A <antonio.anzil@gmail.con>
2018-04-14 17:19:10 +0200

    .

--- modules/movie.py
+++ modules/movie.py
@@ -8,14 +8,14 @@ class Movie:
     @Checks.is_staff()
     @commands.command()
     async def movielist(self, ctx):
-        for author, suggestion in DataManager.read('data/movie.json')[str('movielist')].items():
+        for author, suggestion in DataManager.read('data/movie.json').items():
             embed.add_field(name=str(author), value=str(suggestion), inline=False)
         await ctx.send(embed=embed)
 
     async def on_message(self, message):
         if message.channel.id == 426487069351608330:
-            DataManager.write('data/movie.json', message.author.id, message.content)
-
+            DataManager.list_update('data/movie.json', 'author', str(message.author.name))
+            DataManager.list_update('data/movie.json', 'suggestion', str(message.content))
 
 def setup(bot):
     bot.add_cog(Movie())
