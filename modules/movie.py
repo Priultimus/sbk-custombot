27f5a8af6593ddb38fc0a9c81d895e2e0edd9b6c
Antonio32A <antonio.anzil@gmail.con>
2018-04-14 18:38:05 +0200

    Fix?

--- modules/movie.py
+++ modules/movie.py
@@ -8,7 +8,7 @@ class Movie:
     @Checks.is_staff()
     @commands.command()
     async def movielist(self, ctx):
-        for author, suggestion in DataManager.read('data/movie.json').items():
+        for author, suggestion in DataManager.read('data/movies.json').items():
             author = discord.utils.get(ctx.guild.members, id=int(author))
             embed = discord.Embed(color=ctx.author.color)
             embed.add_field(name=str(author.name), value=str(suggestion), inline=False)
@@ -16,8 +16,8 @@ class Movie:
 
     async def on_message(self, message):
         if message.channel.id == 426487069351608330:
-            DataManager.list_update('data/movie.json', 'author', str(message.author.name))
-            DataManager.list_update('data/movie.json', 'suggestion', str(message.content))
+            DataManager.list_update('data/movies.json', 'author', str(message.author.name))
+            DataManager.list_update('data/movies.json', 'suggestion', str(message.content))
 
 def setup(bot):
     bot.add_cog(Movie())
