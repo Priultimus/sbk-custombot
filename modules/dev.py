b89b290cd1bf05f1387338a3a752c6a59ecd6477
h0r1zon <root@argonbots.me>
2018-04-18 18:01:16 -0400

    O O P S

--- modules/dev.py
+++ modules/dev.py
@@ -49,7 +49,7 @@ class Developer:
             for page in pagify(self.bot._last_exception):
                 await destination.send(box(page, lang="py"))
         else:
-            await Formatter.success("No exception has occurred yet.")
+            await Formatter.success(ctx, "No exception has occurred yet.")
 
     @commands.command()
     @Checks.is_staff()
