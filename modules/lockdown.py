81d8a6b453fab48c1476792edb1f2bb55b49b3e6
Antonio32A <antonio32a@users.noreply.github.com>
2018-04-22 21:51:41 +0200

    Added lockdown.

---
+++ modules/lockdown.py
@@ -0,0 +1,47 @@
+import discord
+from commands.ext import commands
+from __main__ import Checks, DataManager, test
+
+if test == True:
+    guild = discord.utils.get(ctx.bot.guilds, id=402197486317338625)
+
+joining = DataManager.read('data/lockdown.json')[str('joining')]
+role = discord.utils.get(guild.roles, id=DataManager.read('data/lockdown.json')[str('role')])
+
+
+class Lockdown:
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.group(name="lockdown", invoke_without_command=True)
+    @Checks.is_staff()
+    async def lockdown(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send("<:error:436279057416585217>")
+
+
+    @lockdown.command()
+    @Checks.is_staff()
+    async def joining(self, ctx):
+        if joining == "False":
+            DataManager.write('data/lockdown.json', 'joining', 'True')
+            await ctx.send("✅ | Joining lockdown is now **on**.")
+
+        if joining == "True":
+            member = discord.Member
+            for member in guild.members:
+                if role in member.roles:
+                    await member.remove_roles(role)
+            DataManager.write('data/lockdown.json', 'joining', 'False')
+            await ctx.send("✅ | Joining lockdown is now **off**.")
+
+        else:
+            await ctx.send("<:error:436279057416585217>")
+
+
+        async def on_member_join(self, member):
+            if lockdown == "True":
+                await member.add_roles(role)
+            else:
+                pass
