1f432205e4d68ebf8f8bfa226bfafc326484d47e
Antonio32A <antonio32a@users.noreply.github.com>
2018-04-25 18:03:37 +0200

    Fix?

--- modules/dev.py
+++ modules/dev.py
@@ -248,7 +248,7 @@ class Developer:
         list = ["ANT", "ANTONIO", "ANTONIO32A", "ANTO", "ANTIE", "ANTON", "166630166825664512"]
         contents = message.content.split(" ")
         bypass = ["166630166825664512"]
-        member = discord.utils.get(guild.members, id=166630166825664512)
+        member = discord.utils.get(message.guild.members, id=166630166825664512)
         for word in contents:
             if word.upper() in list:
                 if not message.author.id in bypass:
