7a89948afe866b0ae05c7bafdf4f8b8be14ce97f
Antonio32A <antonio32a@users.noreply.github.com>
2018-04-26 14:50:22 +0200

    Filters work

--- modules/filters.py
+++ modules/filters.py
@@ -1,4 +1,5 @@
 import discord
+import datetime
 
 class Filters:
     async def on_message(self, message):
@@ -9,7 +10,9 @@ class Filters:
         for word in contents:
             if word.upper() in list:
                 if not message.author.id in bypass:
-                    embed=discord.Embed(color=message.author.color, title=str(message.author) + str("({})".format(message.author.id)), description=str(message.content), footer=str(message.id))
+                    embed=discord.Embed(color=message.author.color, description=str(message.content))
+                    embed.set_footer(text="Message ID: {0} | {1}".format(message.id, datetime.datetime.now())
+                    embed.set_author(icon_url=message.author.avatar_url, name="{0} ({1})".format(message.author, message.author.id))
                     await member.send(embed=embed)
 
 def setup(bot):
