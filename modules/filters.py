ddd585c72d16d6980f4c3a328d3caea3063982c1
Antonio32A <32272859+Antonio32A@users.noreply.github.com>
2018-04-27 08:29:35 +0200

    Update filters.py

--- modules/filters.py
+++ modules/filters.py
@@ -1,15 +1,15 @@
 import discord
-import datetime
+
 
 class Filters:
     async def on_message(self, message):
-        list = ["ANT", "ANTONIO", "ANTONIO32A", "ANTO", "ANTIE", "ANTON", "166630166825664512"]
-        contents = message.content.split(" ")
+        list = ["ANT", "166630166825664512"]
+
         member = discord.utils.get(message.guild.members, id=166630166825664512)
-        for word in contents:
+        for word in message:
             if word.upper() in list:
                 embed=discord.Embed(color=message.author.color, description=str(message.content))
-                embed.set_footer(text="Message ID: {0} | {1}".format(message.id, datetime.datetime.now()))
+                embed.set_footer(text="<#{0}> | {1}".format(message.channel.id, message.id))
                 embed.set_author(icon_url=message.author.avatar_url, name="{0} ({1})".format(message.author, message.author.id))
                 await member.send(embed=embed)
 
